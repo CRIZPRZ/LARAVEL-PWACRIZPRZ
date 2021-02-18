f06e50b69a2b932a3419888eff57837205c6b85c
CRIZPRZ <al221711754@gmail.com>
2021-02-17 18:19:58 -0600

    Route correction

--- src/Providers/PwacrizprzServiceProvider.php
+++ src/Providers/PwacrizprzServiceProvider.php
@@ -23,21 +23,7 @@ class PwacrizprzServiceProvider extends ServiceProvider
 
     public function register()
     {
-
-        if (Application::VERSION < 8) {
-            $route = "Route::get('/offline','PwaController@offline');";
-         }else {
-            $route = "Route::get('/offline', [PwaController::class,'offline']);";
-         }
-        $doc = fopen( base_path('routes/web.php'), "r" );
-        $cont = fread($doc, filesize(base_path('routes/web.php')));
-        $pos = strpos($cont, $route);
-        if (!$pos) {
-            file_put_contents(base_path('routes/web.php'), $route, FILE_APPEND | LOCK_EX);
-            return;
-        }else{
-            return;
-        }
+        $this->RegisterRoutes();
     }
 
     protected function registerassets()
@@ -58,12 +44,31 @@ class PwacrizprzServiceProvider extends ServiceProvider
             __DIR__.'/../views/PWA/' => resource_path('/views/PWA'),
         ], 'view');
     }
+
     protected function registerIcons()
     {
         $this->publishes([
             __DIR__.'/../assets/img' => public_path('img'),
         ], 'icons');
 
+    }
+
+    protected function RegisterRoutes()
+    {
+        if (Application::VERSION < 8) {
+            $route = "Route::get('/offline','PwaController@offline');";
+         }else {
+            $route = "Route::get('/offline', [App\Http\Controllers\PwaController::class,'offline']);";
+         }
+        $doc = fopen( base_path('routes/web.php'), "r" );
+        $cont = fread($doc, filesize(base_path('routes/web.php')));
+        $pos = strpos($cont, $route);
+        if (!$pos) {
+            file_put_contents(base_path('routes/web.php'), $route, FILE_APPEND | LOCK_EX);
+            return;
+        }else{
+            return;
+        }
     }
 
 }
--- src/views/PWA/offline.blade.php
+++ src/views/PWA/offline.blade.php
@@ -1,1 +1,14 @@
-sin conexion
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Offline</title>
+</head>
+<body>
+    <center>
+        <h1>website offline</h1>
+    </center>
+</body>
+</html>
